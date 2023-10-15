da357aa6dde6af5e10bfe9a46d2a3dfd3d2fb75d
Ishav <106452155+0007ishav@users.noreply.github.com>
2023-10-15 13:52:27 +0530

    Update main.tf

--- for_EachEC2/main.tf
+++ for_EachEC2/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "myEC2Instance" {
  
   ami = var.ami
   instance_type = var.instance_type
-  key_name = var.key_name
 
   subnet_id = var.subnet_id
   # vpc_security_group_ids = var.vpc_security_group_ids
